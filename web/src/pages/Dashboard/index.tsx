0ba5b298a0b9d4687b1fe2f31f54765169a3aaed
tabaesso <tatabaesso@gmail.com>
2021-10-17 11:59:38 -0300

    Add tabela para receber transações na dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -2,15 +2,34 @@ import React from 'react';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, TableContainer } from './styles';
 
 const Dashboard: React.FC = () => {
-  
   return (
     <>
-      <Header size="small" />
+      <Header size="small"/>
       <Container>
-        oi
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="title">Opa</td>
+                <td className="income">Opa</td>
+                <td>Opa</td>
+                <td>Opa</td>
+              </tr>
+            </tbody>
+          </table>
+        </TableContainer>
       </Container>
     </>
   );
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -4,5 +4,50 @@ export const Container = styled.div`
   width: 100%;
   max-width: 1120px;
   margin: 0 auto;
-  padding: 40px 20px;
+  padding: 20px;
+`;
+
+export const TableContainer = styled.section`
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+
+    th {
+      color: #03DAC5;
+      font-weight: normal;
+      padding: 20px 32px;
+      text-align: left;
+      font-size: 16px;
+      line-height: 24px;
+    }
+
+    td {
+      padding: 20px 32px;
+      border: 0;
+      background: #292929;
+      font-size: 16px;
+      font-weight: normal;
+      color: #fff;
+
+      &.title {
+        font-weight: bold;
+      }
+
+      &.income {
+        color: #12a454;
+      }
+
+      &.outcome {
+        color: #e83f5b;
+      }
+    }
+
+    td:first-child {
+      border-radius: 8px 0 0 8px;
+    }
+
+    td:last-child {
+      border-radius: 0 8px 8px 0;
+    }
+  }
 `;
